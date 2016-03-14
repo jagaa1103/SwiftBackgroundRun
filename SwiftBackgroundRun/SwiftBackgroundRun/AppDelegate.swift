ce22ec4eccd646806cacea12ef65b5222ad1d12f
Jagaa <jagaa1103@gmail.com>
2016-03-14 17:30:39 +0900

    edited

--- SwiftBackgroundRun/SwiftBackgroundRun/AppDelegate.swift
+++ SwiftBackgroundRun/SwiftBackgroundRun/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+    var myTask: UIBackgroundTaskIdentifier = 0
+    var myTimer: NSTimer?
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -27,10 +30,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        if checkSupportMultiTaskDevice() == false{
+            return
+        }
+        startRun()
+//        myTask = application.beginBackgroundTaskWithName("myTask1", expirationHandler: {
+//            [weak self] in
+//            self!.endRun()
+//        })
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        self.endRun()
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
@@ -39,6 +52,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func startRun(){
+        myTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "checkBackgroundTime", userInfo: nil, repeats: true)
+    }
+    
+    func endRun(){
+        let main_q = dispatch_get_main_queue()
+        dispatch_async(main_q, {
+            [weak self] in
+            self!.myTimer?.invalidate()
+            self!.myTimer = nil
+            UIApplication.sharedApplication().endBackgroundTask(self!.myTask)
+            self!.myTask = UIBackgroundTaskInvalid
+        })
+    }
+    
+    func checkBackgroundTime(){
+        let backgroundTime = UIApplication.sharedApplication().backgroundTimeRemaining
+        if backgroundTime  != 0.0{
+            print("backgroundTime: \(backgroundTime)")
+        }
+    }
+    
+    func checkSupportMultiTaskDevice()->Bool{
+        return UIDevice.currentDevice().multitaskingSupported
     }
 
 
